286615094da39507b8e038fee0ec9e0759db9c23
Demetrius Araujo <demetriusegundo@gmail.com>
2020-03-27 18:41:56 -0300

    database e controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const OngsController = require('./controllers/OngsController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
+routes.get('/ongs', OngsController.index);
+routes.post('/ongs', OngsController.create);
 
-app.get('/', (req, res)=>{
-  return res.json({msg: "hello World"});
-})
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+
 
 module.exports = routes;
