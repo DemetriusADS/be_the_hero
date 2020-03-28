26fd163c750fb686a410d1e4584bb84b535d5a35
Demetrius Araujo <demetriusegundo@gmail.com>
2020-03-28 15:32:43 -0300

    api integration

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import './styles.css';
 import {FiLogIn} from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
-export default function logon(){
+export default function Logon(){
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e){
+    e.preventDefault();
+    try{
+      const response = await api.post('/session', {id});
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongNome', response.data.nome);
+      console.log(response.data.nome);
+      history.push('profile');
+    }catch(err){
+      alert('Erro no login');
+    }
+  }
+
   return(
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="logo"/>
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID"/>
+          <input placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
 
           <Link to="/register" className="backlink">
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
@@ -7,11 +7,13 @@ import './style.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile(){
+  const ongName = localStorage.getItem('ongNome');
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be the Hero"/>
-        <span>Bem vinda APAD</span>
+          <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new"> Cadastrar novo caso</Link>
         <button type="button">
